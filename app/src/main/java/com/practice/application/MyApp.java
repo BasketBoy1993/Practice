39f939cb1e899edf875fbe2c910944668dc026fb
BasketBoy1993 <939108256@qq.com>
2017-02-22 21:21:01 +0800

    提交

---
+++ app/src/main/java/com/practice/application/MyApp.java
@@ -0,0 +1,18 @@
+package com.practice.application;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/2/22.
+ */
+
+public class MyApp extends Application {
+
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/practice/receiver/MyReceiver.java
@@ -0,0 +1,16 @@
+package com.practice.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Administrator on 2017/2/22.
+ */
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
